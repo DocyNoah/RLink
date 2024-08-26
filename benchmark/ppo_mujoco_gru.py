f8556772bd4dc1c32a8b3dbb57ba20ed954d267b
docynoah <yoweifc@gmail.com>
2024-08-26 23:25:29 +0900

    Add benchmark

---
+++ benchmark/ppo_mujoco_gru.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+import tyro
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+def train_ppo_mujoco_gru() -> None:
+    from rlink.trainers.mujoco.ppo_mujoco_rnn import Agent, Args, train_ppo
+    args = tyro.cli(Args)
+    train_ppo(args, Agent)
+
+
+if __name__ == "__main__":
+    train_ppo_mujoco_gru()
