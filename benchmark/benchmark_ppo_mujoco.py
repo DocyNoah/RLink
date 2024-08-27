9dbbe6a2c1fd70427d548c6d31bd80b80b89a230
docynoah <yoweifc@gmail.com>
2024-08-27 18:07:37 +0900

    Rename files

--- benchmark/benchmark_ppo_mujoco.py
+++ benchmark/benchmark_ppo_mujoco.py
@@ -6,12 +6,12 @@ import tyro
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
-def train_ppo_mujoco_gru() -> None:
-    from rlink.trainers.mujoco.ppo_mujoco_rnn import Agent, Args, train_ppo
+def train_ppo_mujoco() -> None:
+    from rlink.trainers.mujoco.train_ppo_mujoco import Agent, Args, train_ppo
 
     args = tyro.cli(Args)
     train_ppo(args, Agent)
 
 
 if __name__ == "__main__":
-    train_ppo_mujoco_gru()
+    train_ppo_mujoco()
