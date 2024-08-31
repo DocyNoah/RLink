a8d3dcfd5adb9aa87efda83c58df154c3497f0be
docynoah <yoweifc@gmail.com>
2024-08-31 19:45:41 +0900

    Add POMDP envs

---
+++ rlink/envs/momdp_mujoco_envs.py
@@ -0,0 +1,74 @@
+import gymnasium as gym
+
+from rlink.envs.wrappers.momdp_wrapper import POMDPWrapper
+
+
+class POMDPMujocoEnv:
+    """
+    Use this class to register POMDP environments in this file.
+
+    Example:
+    from rlink.envs.momdp_mujoco_envs import POMDPMujocoEnv
+    gym.register_envs(POMDPMujocoEnv)
+    """
+    pass
+
+
+# HalfCheetah-v4
+# https://gymnasium.farama.org/environments/mujoco/half_cheetah/#observation-space
+def HalfCheetah_P_factory() -> gym.Env:
+    env_id = "HalfCheetah-v4"
+    visible_obs_dim = [0, 1, 2, 3, 4, 5, 6, 7]
+    env = POMDPWrapper(env_id, visible_obs_dim)
+    return env
+
+
+def HalfCheetah_V_factory() -> gym.Env:
+    env_id = "HalfCheetah-v4"
+    visible_obs_dim = [8, 9, 10, 11, 12, 13, 14, 15, 16]
+    env = POMDPWrapper(env_id, visible_obs_dim)
+    return env
+
+
+gym.register(id="HalfCheetahP-v4", entry_point=HalfCheetah_P_factory)
+gym.register(id="HalfCheetahV-v4", entry_point=HalfCheetah_V_factory)
+
+
+# Hopper-v4
+# https://gymnasium.farama.org/environments/mujoco/hopper/#observation-space
+def Hopper_P_factory() -> gym.Env:
+    env_id = "Hopper-v4"
+    visible_obs_dim = [0, 1, 2, 3, 4]
+    env = POMDPWrapper(env_id, visible_obs_dim)
+    return env
+
+
+def Hopper_V_factory() -> gym.Env:
+    env_id = "Hopper-v4"
+    visible_obs_dim = [5, 6, 7, 8, 9, 10]
+    env = POMDPWrapper(env_id, visible_obs_dim)
+    return env
+
+
+gym.register(id="HopperP-v4", entry_point=Hopper_P_factory)
+gym.register(id="HopperV-v4", entry_point=Hopper_V_factory)
+
+
+# Walker2d-v4
+# https://gymnasium.farama.org/environments/mujoco/walker2d/#observation-space
+def Walker2D_P_factory() -> gym.Env:
+    env_id = "Walker2D-v4"
+    visible_obs_dim = [0, 1, 2, 3, 4, 5, 6, 7]
+    env = POMDPWrapper(env_id, visible_obs_dim)
+    return env
+
+
+def Walker2D_V_factory() -> gym.Env:
+    env_id = "Walker2D-v4"
+    visible_obs_dim = [8, 9, 10, 11, 12, 13, 14, 15, 16]
+    env = POMDPWrapper(env_id, visible_obs_dim)
+    return env
+
+
+gym.register(id="Walker2DP-v4", entry_point=Walker2D_P_factory)
+gym.register(id="Walker2DV-v4", entry_point=Walker2D_V_factory)
---
+++ rlink/envs/wrappers/momdp_wrapper.py
@@ -0,0 +1,20 @@
+import gymnasium as gym
+from gymnasium.core import ObsType
+
+
+class POMDPWrapper(gym.ObservationWrapper):
+    def __init__(self, env_id: str, visible_obs_dim: list[int]):
+        env = gym.make(env_id)
+        super().__init__(env=env)
+        assert isinstance(visible_obs_dim, list)
+
+        self._visible_obs_dim = visible_obs_dim
+
+        self.observation_space = gym.spaces.Box(
+            low=self.env.observation_space.low[self._visible_obs_dim],
+            high=self.env.observation_space.high[self._visible_obs_dim],
+            dtype=self.env.observation_space.dtype,
+        )
+
+    def observation(self, observation: ObsType) -> ObsType:
+        return observation[self._visible_obs_dim]
